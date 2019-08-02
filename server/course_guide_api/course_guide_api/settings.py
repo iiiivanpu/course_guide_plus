9848770296fd8d58ff8076b849747f2fa66edf4e
ivanpu <ivanpu@umich.edu>
2019-08-02 16:04:46 +0800

    added aws mysql
    Signed-off-by: ivanpu <ivanpu@umich.edu>

--- server/course_guide_api/course_guide_api/settings.py
+++ server/course_guide_api/course_guide_api/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'course_guide_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'courses_info',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
