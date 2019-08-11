519f3d425707ace0978b6173eb067da6b06c04dc
ivanpu <ivanpu@umich.edu>
2019-08-11 20:15:32 +0800

    deployed api onto aws elastic beanstalk
    Signed-off-by: ivanpu <ivanpu@umich.edu>

--- server/course_guide_api/course_guide_api/settings.py
+++ server/course_guide_api/course_guide_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2a@$r&b2+2498gnghp7#5bxa&c5afe@+lkwod@on(*ze!gl0-y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.bvi52yefg9.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
