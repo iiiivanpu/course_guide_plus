5a9e60083e3f8d83dd577bece35e764d7fe3fad3
Ken Zhang <kenzhang98@hotmail.com>
2019-08-11 14:11:58 -0400

    Add skeleton for routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
 import WelcomePage from "./welcome-page";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
+  Home = () => {
+    return (
+      <div>
+        <h2>Home</h2>
+      </div>
+    );
+  };
+
+  About = () => {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  };
+
   render() {
     return (
-      <div className="app">
-        <WelcomePage />
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/course_info">About</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={WelcomePage} />
+          <Route path="/course_info" component={this.About} />
+        </div>
+      </Router>
     );
   }
 }
