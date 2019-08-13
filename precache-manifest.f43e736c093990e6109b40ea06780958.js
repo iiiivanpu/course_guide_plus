b2dc0df67d3020550e69342e1a7416a13ce93817
Ken Zhang <kenzhang98@hotmail.com>
2019-08-13 19:23:48 -0400

    Updates

---
+++ precache-manifest.f43e736c093990e6109b40ea06780958.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8990f8489f5c79160349a1f6c183c2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bea911ee61ca8553879",
+    "url": "/static/js/2.4317ac99.chunk.js"
+  },
+  {
+    "revision": "dd6a875152b1e8b55d1d",
+    "url": "/static/js/main.13e790c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "314b5011cce5ee3309b7eb0d8448151d",
+    "url": "/static/media/logo.314b5011.png"
+  }
+]);
