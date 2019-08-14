38a1eb825f68b974a6a862f6a3aecff07af3e9d6
ivanpu <ivanpu@umich.edu>
2019-08-14 21:28:32 +0800

    changed api path
    Signed-off-by: ivanpu <ivanpu@umich.edu>

--- server/course_guide_api/api/urls.py
+++ server/course_guide_api/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ClassView
 
 urlpatterns = [
-    path('classes/<str:dept>/<str:code>', ClassView.as_view(), name="class_all")
+    path('<str:dept>/<str:code>', ClassView.as_view(), name="class_all")
 ]
--- server/course_guide_api/course_guide_api/urls.py
+++ server/course_guide_api/course_guide_api/urls.py
@@ -21,5 +21,5 @@ from rest_framework import routers, serializers, viewsets
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('classes/', include('api.urls'))
 ]
