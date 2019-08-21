8e0736c79278149abcc9fa50196cce9f311d0c1f
Ken Zhang <kenzhang98@hotmail.com>
2019-08-20 21:52:39 -0400

    Add social sharing buttons

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import UrlSync from './url-sync';
 import backgroundImage from '../static/background.jpg';
+import { InlineShareButtons } from 'sharethis-reactjs';
 
 const BackgroundContainer = styled('div', {
   position: 'absolute',
@@ -23,11 +24,12 @@ const BackgroundContainer = styled('div', {
 const TopbarContainer = styled('div', {
   display: 'flex',
   flexDirection: 'row-reverse',
+  marginRight: '25px',
 });
 const TopbarElementContainer = styled('div', {
   color: 'white',
-  fontSize: '30px',
-  marginRight: '50px',
+  fontSize: '25px',
+  marginLeft: '50px',
   marginTop: '7px',
 });
 const linkStyle = {
@@ -38,6 +40,19 @@ const StyledAnchor = styled('a', {
   ':hover': {
     color: '#FFCB05',
   },
+});
+const SocialShareContainer = styled('a', {
+  position: 'absolute',
+  bottom: '0',
+  right: '0',
+  display: 'flex',
+  alignItems: 'center',
+});
+const SocialShareMessage = styled('div', {
+  color: 'white',
+  marginRight: '10px',
+  fontFamily: 'Arial, Helvetica, sans-serif',
+  fontSize: '18px',
 });
 
 class App extends Component {
@@ -92,7 +107,6 @@ class App extends Component {
                   <StyledAnchor>About</StyledAnchor>
                 </Link>
               </TopbarElementContainer>
-
               <TopbarElementContainer>
                 <Link
                   to="/"
@@ -104,12 +118,45 @@ class App extends Component {
               </TopbarElementContainer>
             </TopbarContainer>
             <hr />
-
             <Route exact path="/" component={MainPage} />
             <Route path="/about" component={this.About} />
             <Route path="/contact" component={this.Contact} />
           </BackgroundContainer>
         </Router>
+        <SocialShareContainer>
+          <SocialShareMessage>Share to Others</SocialShareMessage>
+          <InlineShareButtons
+            config={{
+              alignment: 'right', // alignment of buttons (left, center, right)
+              color: 'social', // set the color of buttons (social, white)
+              enabled: true, // show/hide buttons (true, false)
+              font_size: 16, // font size for the buttons
+              labels: 'null', // button labels (cta, counts, null)
+              language: 'en', // which language to use (see LANGUAGES)
+              networks: [
+                // which networks to include (see SHARING NETWORKS)
+                'whatsapp',
+                'linkedin',
+                'messenger',
+                'facebook',
+                'wechat',
+                'twitter',
+              ],
+              padding: 12, // padding within buttons (INTEGER)
+              radius: 4, // the corner radius on each button (INTEGER)
+              show_total: false,
+              size: 40, // the size of each button (INTEGER)
+
+              // OPTIONAL PARAMETERS
+              url: 'https://course-guide-plus.ml/', // (defaults to current url)
+              //image: 'https://bit.ly/2CMhCMC', // (defaults to og:image or twitter:image)
+              description:
+                'Course Guide Plus is an easier and faster way to find courses at the Unviersity of Michigan ', // (defaults to og:description or twitter:description)
+              title: 'Course Guide Plus', // (defaults to og:title or twitter:title)
+              //username: 'custom twitter handle', // (only for twitter sharing)
+            }}
+          />
+        </SocialShareContainer>
       </React.Fragment>
     );
   }
